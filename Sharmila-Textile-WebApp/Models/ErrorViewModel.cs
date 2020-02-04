027f3b6868b0b924714ec17dfdc22b92c5a2fab4
Salman <Salman.primary@gmail.com>
2020-02-04 13:50:55 +0530

    Add project files.

---
+++ Sharmila-Textile-WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Sharmila_Textile_WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
