d1d86f8be023d6ae267aaaebffdbffed26d09bbd
dushyanthkandiah <41447256+dushyanthkandiah@users.noreply.github.com>
2020-02-05 21:20:14 +0530

    bootstrap active class

---
+++ Sharmila-Textile-WebApp/HtmlEntities/ActiveRouteTagHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.AspNetCore.Routing;
+
+namespace Sharmila_Textile_WebApp.HtmlEntities {
+
+    public static class ActiveRouteTagHelper {
+        public static string IsTabActive(ViewContext viewContext, string selectedController, string selectedAction) {
+            string flag = "";
+
+            string controller = viewContext.RouteData.Values["controller"].ToString();
+            string action = viewContext.RouteData.Values["action"].ToString();
+
+            if (controller == selectedController && action == selectedAction) {
+                flag = "active";
+            }
+
+            return flag;
+        }
+    }
+}
---
+++ Sharmila-Textile-WebApp/Models/Staff.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sharmila_Textile_WebApp.Models {
+
+    [Table("STAFF")]
+    public class Staff {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("STAFF_ID")]
+        public long StaffId { get; set; }
+
+        [StringLength(50, ErrorMessage = "Staff Name must not be longer than 50 characters")]
+        [Column("STAFF_NAME")]
+        public string StaffName { get; set; }
+         
+        [Column("NIC", TypeName = "varchar(max)")]
+        public string Nic { get; set; }
+
+
+    }
+}
